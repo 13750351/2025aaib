f960a094ce11539c8d5900f67a8944313aaae033
13750351 <13750351mcu@gmail.com>
2025-02-19 11:55:21 +0800

    week01

---
+++ week01-1.cpp
@@ -0,0 +1,16 @@
+/// week01-1.cpp 瘋狂程設
+///File-New empty 開新的檔案
+///Ctrl-s 存檔成 week01-1.cpp 要記得加.cpp
+///下周考試考這題，不用寫藍色的「中文註解」
+# include <stdio.h>///檔名不能寫錯
+///stdio的意思是標準的standard input/output
+///有了stdio.h的外掛，才能用scanf() print()
+int main()
+
+{
+    int a,b;  ///整數a,b
+    scanf("%d" , &a );  ///
+    scanf("%d" , &b );
+    printf("%d" , a+b);
+
+}
