d6e8265571731bc05f6d489fd2f2610e9fa8f020
13750351 <13750351mcu@gmail.com>
2025-04-16 12:07:43 +0800

    week09

---
+++ week09-3.cpp
@@ -0,0 +1,24 @@
+///week09-3.cpp
+///TEH001_Star
+#include <stdio.h>
+int main()
+{
+	printf("請輸入大小:")
+	int n;
+	scanf("%d",&n);
+
+	foe(int i=1;i<=n;i++){
+        for(int j=1; j<=i; j++){
+            pritnf("*");
+
+	printf("i::\n",i)
+        }
+
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			printf("*");
+	}
+	printf("\n", i);
+	}
+
+}
